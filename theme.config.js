24cce02ad6f1ff23727aaac4b2c4e8ce5d551aab
David Edelhart <dave@wonderlandlabs.com>
2022-03-16 22:13:47 -0700

    work on examples;

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/bingomanate/forest',
+  docsRepositoryBase: 'https://github.com/bingomanatee/forest-docs',
+  titleSuffix: ' – Forest',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Forest</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        The state management engine for real world apps
       </span>
     </>
   ),
@@ -16,15 +16,16 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
-      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="description" content="Forest: state management for real world apps" />
+      <meta name="og:description" content="Forest: State Management" />
+      {/*  <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
       <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="twitter:url" content="https://nextra.vercel.app" /> */ }
+      <meta name="og:title" content="Forest: State Management" />
+      { /* <meta name="og:image" content="https://nextra.vercel.app/og.png" /> */ }
+      <meta name="apple-mobile-web-app-title" content="Forest" />
+      <link rel="stylesheet" href="/example-style.css"/>
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -62,6 +63,6 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>MIT {new Date().getFullYear()} © Wonderland Labs.</>,
   unstable_faviconGlyph: '👋',
 }
